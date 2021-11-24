ce478c61ecc87a2952bcd6a7d030fb70960ae537
ROL <romanlopatin@gmail.com>
2021-11-24 20:43:40 +0300

    Lesson 2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,15 +1,84 @@
+import json
+
 from django.shortcuts import render
+
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "mainapp/index.html")
+    context = {'title': 'магазин'}
+    return render(request, "mainapp/index.html", context)
+
+
+links_menu = [
+    {'href': "products", 'name': 'все'},
+    {'href': "products_home", 'name': 'дом'},
+    {'href': "products_office", 'name': 'офис'},
+    {'href': "products_modern", 'name': 'модерн'},
+    {'href': "products_classic", 'name': 'классика'}
+]
 
 
 def products(request):
-    return render(request, "mainapp/products.html")
+    context = {
+        'title': 'каталог',
+        'links_menu': links_menu
+    }
+    return render(request, "mainapp/products.html", context)
+
+
+def products_home(request):
+    context = {
+        'title': 'каталог',
+        'links_menu': links_menu
+    }
+    return render(request, "mainapp/products.html", context)
+
+
+def products_office(request):
+    context = {
+        'title': 'каталог',
+        'links_menu': links_menu
+    }
+    return render(request, "mainapp/products_.html", context)
+
+
+def products_modern(request):
+    context = {
+        'title': 'каталог',
+        'links_menu': links_menu
+    }
+    return render(request, "mainapp/products.html", context)
+
+
+def products_classic(request):
+    context = {
+        'title': 'каталог',
+        'links_menu': links_menu
+    }
+    return render(request, "mainapp/products.html", context)
 
 
 def contact(request):
-    return render(request, "mainapp/contact.html")
+    with open("json/contact_info.json") as read_f:
+        adresses = json.load(read_f)
+
+    context = {
+        'title': 'Контакты',
+        'addresses': adresses
+    }
+    return render(request, "mainapp/contact.html", context)
+
+
+def context(request):
+    with open("json/my.json") as read_f:
+        some_info = json.load(read_f)
+
+    context = {
+        'title': "Title",
+        'header': "header",
+        'username': "Джон",
+        'products': [{'name': 'one', 'price': 1}, {'name': 'two', 'price': 10}, {'name': 'three', 'price': 15}]
+    }
+    return render(request, "mainapp/test_context.html", context)
