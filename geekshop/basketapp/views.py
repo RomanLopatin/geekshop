94ab01861411894d74fe8d4ae3a4f5fec0de5140
ROL <romanlopatin@gmail.com>
2021-12-13 13:48:44 +0300

    lesson-6 (part 2) 1-4+6++

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from basketapp.models import Basket
 from mainapp.models import Product
@@ -21,6 +22,8 @@ def basket(request):
 
 @login_required
 def basket_add(request, pk):
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:product', args=[pk]))
     product = get_object_or_404(Product, pk=pk)
 
     basket = Basket.objects.filter(user=request.user, product=product).first()
